f81b3f40a885862ecd06ac4ab2bbc47d001d2e7f
Matthew Neiman <mneiman77@gmail.com>
2020-03-22 14:40:08 +0100

    Broke up Init into more natural flow

--- Problema_1_6.py
+++ Problema_1_6.py
@@ -1,5 +1,4 @@
 import numpy as np 
-import matplotlib.pyplot as plt
 from scipy.constants import speed_of_light, epsilon_0, mu_0
 
 eta_0 = np.sqrt(mu_0 / epsilon_0)
@@ -12,16 +11,24 @@ class PyPanel():
     omega = np.linspace(fIni,fEnd,N) * 2 * np.pi
 
     def __init__(self, epsilon_r = 1.0 , mu_r = 1.0, sigma = 0.0, d = 100e-6):
+        self.sigma = sigma
+        self.d = d
         self.mu = mu_r * mu_0
         self.epsilon_c = -np.complex(0,1)*sigma/PyPanel.omega + \
                          epsilon_0 * epsilon_r
-        self.gamma = np.complex(0,1) * PyPanel.omega * np.sqrt(self.mu * self.epsilon_c)
-        self.gd = self.gamma * d
         self.eta = np.sqrt(self.mu / self.epsilon_c)
-        self.phi = np.array([[np.cosh(self.gd),self.eta*np.sinh(self.gd)], \
-                             [np.sinh(self.gd)/self.eta,np.cosh(self.gd)]])
-        self.denom = self.phi[0,0] * eta_0 + self.phi[0,1] + \
-                     self.phi[1,0] * eta_0**2 + self.phi[1,1] * eta_0
+        self.phi = self._phi()
+        self.denom = self._denom()
+
+    def _phi(self):
+        gamma = np.complex(0,1) * PyPanel.omega * np.sqrt(self.mu * self.epsilon_c)
+        gd = gamma * self.d
+        return np.array([[np.cosh(gd),self.eta*np.sinh(gd)], \
+                         [np.sinh(gd)/self.eta,np.cosh(gd)]])
+
+    def _denom(self):
+        return self.phi[0,0] * eta_0 + self.phi[0,1] + \
+               self.phi[1,0] * eta_0**2 + self.phi[1,1] * eta_0
 
     def T(self):
         return 2 * self.eta / self.denom
